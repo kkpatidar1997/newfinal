417a2a4663377d9be09c6fa22609be86b5f58c7a
kkpatidar1997 <kkpatidar1997@gmail.com>
2024-06-18 22:34:32 +0530

    updat index file

--- Full_Stack_Project_EntertainmentApp/server/index.js
+++ Full_Stack_Project_EntertainmentApp/server/index.js
@@ -1,7 +1,38 @@
+// import express from "express";
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
+// import http from "http";
+// import mongoose from "mongoose";
+// import "dotenv/config";
+// import routes from "./src/routes/index.js";
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+
+// app.use("/api/v1", routes);
+
+// const port = process.env.PORT || 5000;
+
+// const server = http.createServer(app);
+
+// mongoose.connect(process.env.MONGODB_URL).then(() => {
+//   console.log("Mongodb connected");
+//   server.listen(port, () => {
+//     console.log(`Server is listening on port ${port}`);
+//   });
+// }).catch((err) => {
+//   console.log({ err });
+//   process.exit(1);
+// });
+
+// //test
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import http from "http";
 import mongoose from "mongoose";
 import "dotenv/config";
 import routes from "./src/routes/index.js";
@@ -17,16 +48,15 @@ app.use("/api/v1", routes);
 
 const port = process.env.PORT || 5000;
 
-const server = http.createServer(app);
-
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-  console.log("Mongodb connected");
-  server.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Mongodb connected");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+    process.exit(1);
   });
-}).catch((err) => {
-  console.log({ err });
-  process.exit(1);
-});
 
-//test
+// Export the app for Vercel
+export default app;
